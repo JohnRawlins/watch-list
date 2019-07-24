22533addc12f51cfeae1c276c486301eb5136278
John Rawlins <>
2019-07-24 17:53:42 -0400

    Adjusted folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SignIn from './SignIn.js';
-import '../components/styling/css/app.scss';
-
+import '../css/app.scss';
 
 function App() {
   return (
     <div className="app">
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/login" component={SignIn} />
-      </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={SignIn} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
