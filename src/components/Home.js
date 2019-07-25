532e3fff2dafa2453277dc6c6ad0f09ecac84c9b
John Rawlins <>
2019-07-25 01:06:44 -0400

    Created Navbar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './Navbar.js'
 import '../css/home.scss';
 
 const Home = () => {
     return(
          <div className="home">
-           
+           <Navbar />
          </div>
     )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../css/navbar.scss';
+import watchListLogo from '../img/watchlist-logo.svg';
+import menuIcon from '../img/hamburger-menu.svg';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img
+          className="navbar-logo__img"
+          src={watchListLogo}
+          alt="Smiling TV"
+        />
+        <h1 className="navbar-logo__title">Watch List</h1>
+      </div>
+      <button className="navbar__menu-btn">
+        <img
+          className="navbar__menu-icon"
+          src={menuIcon}
+          alt="Hamburger Menu"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
