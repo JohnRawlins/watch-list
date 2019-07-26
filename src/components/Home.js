b02e45f7b4cc9bc094641fbffe323ef76921860a
John Rawlins <john.rawlins2@outlook.com>
2019-07-25 20:52:55 -0400

    Created search field in Home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navbar from './Navbar.js';
+import searchIcon from '../img/search-icon.svg';
 import '../css/home.scss';
+
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <form className="search">
+      <input className="search__input" type="text" placeholder="Search"/>
+      <button type="submit" className="search__btn">
+        <img className="search__btn-img" src={searchIcon} alt=""/>
+      </button>
+      </form>
     </div>
   );
 };
