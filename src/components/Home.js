5539a3a71e660c51ae37e925931a67a2eb5be141
John Rawlins <john.rawlins2@outlook.com>
2019-07-25 22:35:25 -0400

    Added search results container

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import Navbar from './Navbar.js';
 import searchIcon from '../img/search-icon.svg';
+import popcorn from '../img/popcorn.svg';
 import '../css/home.scss';
-
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <form className="search">
-      <input className="search__input" type="text" placeholder="Search"/>
-      <button type="submit" className="search__btn">
-        <img className="search__btn-img" src={searchIcon} alt=""/>
-      </button>
+        <input className="search__input" type="text" placeholder="Search" />
+        <button className="search__btn" type="submit">
+          <img
+            className="search__btn-img"
+            src={searchIcon}
+            alt="Search Button"
+          />
+        </button>
       </form>
+      <div className="search-result">
+        <div className="search-default">
+          <img className="search-default__img" src={popcorn} alt="Container of Popcorn"/>
+          <p className="search-default__message">Search For Movies or Shows</p>
+        </div>
+      </div>
     </div>
   );
 };
