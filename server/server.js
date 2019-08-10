d469b4930b7e62fee11529f2fc58f947e9f7a10d
John Rawlins <john.rawlins2@outlook.com>
2019-08-10 02:04:58 -0400

    Created Auth and Users Routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hey'));
 
-const PORT = process.env.PORT || 5000;
+app.use('/api/users', require('./routes/users'));
+
+app.use('/api/auth', require('./routes/auth'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
