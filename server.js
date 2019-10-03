995653c1254ef06b01b584ce4cc6cb89274b6f94
John Rawlins <john.rawlins2@outlook.com>
2019-10-03 03:53:35 -0400

    Deployment Prep

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 const connectToDB = require('./config/db');
 
@@ -18,5 +19,12 @@ app.use('/api/reviews', require('./routes/reviews'));
 app.use('/api/search', require('./routes/search'));
 
 app.use('/api/video-profile', require('./routes/video-profile'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
