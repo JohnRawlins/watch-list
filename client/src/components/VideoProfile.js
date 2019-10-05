69f1842a51c65d230ef84e438fa791fc6a799165
John Rawlins <john.rawlins2@outlook.com>
2019-10-05 15:02:22 -0400

    Placeholder Images For VideoProfile

--- client/src/components/VideoProfile.js
+++ client/src/components/VideoProfile.js
@@ -10,6 +10,7 @@ import reviewStar from '../img/review-star.svg';
 import freshScore from '../img/fresh-score.png';
 import rottenScore from '../img/rotten-score.png';
 import UserReviews from './UserReviews';
+import VideoProfilePlaceHolder from './VideoProfilePlaceHolder';
 
 const VideoProfile = ({ location }) => {
   const [video, setVideo] = useState({});
@@ -22,6 +23,8 @@ const VideoProfile = ({ location }) => {
     getVideoReviews,
     userReviews
   } = useContext(ReviewContext);
+
+  const [isLoading, setLoading] = useState(true);
 
   const getVideoProfile = async () => {
     let rottenTomatoesScore;
@@ -49,6 +52,8 @@ const VideoProfile = ({ location }) => {
       getVideoReviews(videoProfilePayload.imdbID);
 
       setVideo({ ...videoProfilePayload });
+
+      setLoading(false);
     } catch (error) {
       console.error(error);
     }
@@ -59,103 +64,111 @@ const VideoProfile = ({ location }) => {
     //eslint-disable-next-line
   }, [infoModalMsg]);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="video-profile-container">
       <Navbar />
       {writeReviewModal.visible && <WriteReview videoInfo={video} />}
-      <div className="video-profile">
-        <section className="video-details">
-          <img
-            className="video-details__poster"
-            src={video.Poster}
-            alt="video Poster"
-            onError={event => {
-              event.target.src = defaultPoster;
-            }}
-          />
-          <div className="video-details-general">
-            <h1 className="video-details-general__title">{video.Title}</h1>
-            <p className="video-details-general__release-date">
-              {video.Released}
-            </p>
-            <p className="video-details-general__runtime">{video.Runtime}</p>
-            <p className="video-details-general__age-rating">{video.Rated}</p>
-            <div className="video-details-rt">
-              <p className="video-details-rt__tomatometer-heading">
-                Tomatometer
+      {isLoading ? (
+        <VideoProfilePlaceHolder />
+      ) : (
+        <div className="video-profile">
+          <section className="video-details">
+            <img
+              className="video-details__poster"
+              src={video.Poster}
+              alt="video Poster"
+              onError={event => {
+                event.target.src = defaultPoster;
+              }}
+            />
+            <div className="video-details-general">
+              <h1 className="video-details-general__title">{video.Title}</h1>
+              <p className="video-details-general__release-date">
+                {video.Released}
               </p>
-              <div className="tomatometer">
-                <img
-                  className="tomatometer__icon"
-                  src={
-                    video.rottenTomatoesScore >= 60
-                      ? freshScore
-                      : video.rottenTomatoesScore < 60
-                      ? rottenScore
-                      : defaultScore
-                  }
-                  alt="Rotten Tomatoes"
-                />
-                <span className="tomatometer__rating">
-                  {video.rottenTomatoesScore}
-                  {typeof video.rottenTomatoesScore === 'number' ? '%' : ''}
-                </span>
-              </div>
-            </div>
-          </div>
-        </section>
-        <button
-          onClick={() => addVideoToWatchList(video)}
-          className="video-profile__watch-list-btn"
-        >
-          Add To Watch List
-        </button>
-        <section className="video-plot">
-          <h2 className="video-plot__heading">Plot</h2>
-          <p className="video-plot__text">{video.Plot}</p>
-        </section>
-        <section className="video-credits">
-          <h2 className="video-credits__heading">Cast and Credits</h2>
-          <div className="video-actors">
-            <h3 className="video-actors__heading">Actors</h3>
-            <p className="video-actors__info">{video.Actors}</p>
-          </div>
-          <div className="video-director">
-            <h3 className="video-director__heading">Director</h3>
-            <p className="video-director__info">{video.Director}</p>
-          </div>
-        </section>
-        <section className="user-reviews">
-          <h2 className="user-reviews__heading">User Reviews</h2>
-          <div className="video-review-container">
-            <div className="video-review">
-              <div className="video-review-score-container">
-                <span className="video-review__score">
-                  {userReviews.userReviewScore}
-                </span>
-                <div className="user-reviews-total">
-                  <p className="user-reviews-total__heading">Total</p>
-                  <span className="user-reviews-total__num">
-                    {userReviews.totalUserReviews}
+              <p className="video-details-general__runtime">{video.Runtime}</p>
+              <p className="video-details-general__age-rating">{video.Rated}</p>
+              <div className="video-details-rt">
+                <p className="video-details-rt__tomatometer-heading">
+                  Tomatometer
+                </p>
+                <div className="tomatometer">
+                  <img
+                    className="tomatometer__icon"
+                    src={
+                      video.rottenTomatoesScore >= 60
+                        ? freshScore
+                        : video.rottenTomatoesScore < 60
+                        ? rottenScore
+                        : defaultScore
+                    }
+                    alt="Rotten Tomatoes"
+                  />
+                  <span className="tomatometer__rating">
+                    {video.rottenTomatoesScore}
+                    {typeof video.rottenTomatoesScore === 'number' ? '%' : ''}
                   </span>
                 </div>
               </div>
-              <img
-                className="video-review__star"
-                src={reviewStar}
-                alt="Review Star"
-              />
-              <button
-                onClick={() => setWriteReviewModal(true)}
-                className="video-review__btn"
-              >
-                Write A Review
-              </button>
             </div>
-          </div>
-          <UserReviews videoReviews={userReviews} />
-        </section>
-      </div>
+          </section>
+          <button
+            onClick={() => addVideoToWatchList(video)}
+            className="video-profile__watch-list-btn"
+          >
+            Add To Watch List
+          </button>
+          <section className="video-plot">
+            <h2 className="video-plot__heading">Plot</h2>
+            <p className="video-plot__text">{video.Plot}</p>
+          </section>
+          <section className="video-credits">
+            <h2 className="video-credits__heading">Cast and Credits</h2>
+            <div className="video-actors">
+              <h3 className="video-actors__heading">Actors</h3>
+              <p className="video-actors__info">{video.Actors}</p>
+            </div>
+            <div className="video-director">
+              <h3 className="video-director__heading">Director</h3>
+              <p className="video-director__info">{video.Director}</p>
+            </div>
+          </section>
+          <section className="user-reviews">
+            <h2 className="user-reviews__heading">User Reviews</h2>
+            <div className="video-review-container">
+              <div className="video-review">
+                <div className="video-review-score-container">
+                  <span className="video-review__score">
+                    {userReviews.userReviewScore}
+                  </span>
+                  <div className="user-reviews-total">
+                    <p className="user-reviews-total__heading">Total</p>
+                    <span className="user-reviews-total__num">
+                      {userReviews.totalUserReviews}
+                    </span>
+                  </div>
+                </div>
+                <img
+                  className="video-review__star"
+                  src={reviewStar}
+                  alt="Review Star"
+                />
+                <button
+                  onClick={() => setWriteReviewModal(true)}
+                  className="video-review__btn"
+                >
+                  Write A Review
+                </button>
+              </div>
+            </div>
+            <UserReviews videoReviews={userReviews} />
+          </section>
+        </div>
+      )}
     </div>
   );
 };
---
+++ client/src/components/VideoProfilePlaceHolder.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../css/video-profile-place-holder.scss';
+
+const VideoProfilePlaceHolder = () => {
+  return (
+    <div className="video-profile-plc-holder-container">
+      <div className="video-profile-plc-holder">
+        <div className="profile-plc-holder-main">
+          <div className="profile-plc-holder-main__poster"></div>
+          <div className="video-profile-plc-holder-content">
+            <div className="video-profile-plc-holder-content__sent"></div>
+            <div className="video-profile-plc-holder-content__sent"></div>
+            <div className="video-profile-plc-holder-content__sent"></div>
+            <div className="video-profile-plc-holder-content__sent"></div>
+            <div className="video-profile-plc-holder-content__sent"></div>
+          </div>
+        </div>
+        <div className="video-profile-plc-holder-content">
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+        </div>
+
+        <div className="video-profile-plc-holder-content">
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+        </div>
+
+        <div className="video-profile-plc-holder-content">
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+          <div className="video-profile-plc-holder-content__sent"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoProfilePlaceHolder;
