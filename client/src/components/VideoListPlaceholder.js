fef6ca81e8ccf94e8da6577a8af54f24a5ec73d4
John Rawlins <john.rawlins2@outlook.com>
2019-11-10 05:41:58 -0500

    Styled placeholder for PopularVideos Component

--- client/src/components/VideoListPlaceholder.js
+++ client/src/components/VideoListPlaceholder.js
@@ -3,32 +3,32 @@ import '../css/video-list-placeholder.scss';
 
 const VideoListPlaceHolder = () => {
   return (
-    <>
+    <div className="video-list-plc-hldr-container">
       <div className="video-list-plc-hldr">
-        <div className="video-list-plc-hldr-poster"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
+        <div className="video-list-plc-hldr__poster"></div>
+        <div className="video-list-plc-hldr__details"></div>
+        <div className="video-list-plc-hld__details"></div>
+        <div className="video-list-plc-hldr__details"></div>
       </div>
       <div className="video-list-plc-hldr">
-        <div className="video-list-plc-hldr-poster"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
+        <div className="video-list-plc-hldr__poster"></div>
+        <div className="video-list-plc-hldr__details"></div>
+        <div className="video-list-plc-hldr__details"></div>
+        <div className="video-list-plc-hldr__details"></div>
       </div>
       <div className="video-list-plc-hldr">
-        <div className="video-list-plc-hldr-poster"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
+        <div className="video-list-plc-hldr__poster"></div>
+        <div className="video-list-plc-hldr__details"></div>
+        <div className="video-list-plc-hldr__details"></div>
+        <div className="video-list-plc-hldr__details"></div>
       </div>
       <div className="video-list-plc-hldr">
-        <div className="video-list-plc-hldr-poster"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
-        <div className="video-list-plc-hldr-details"></div>
+        <div className="video-list-plc-hldr__poster"></div>
+        <div className="video-list-plc-hldr__details"></div>
+        <div className="video-list-plc-hldr__details"></div>
+        <div className="video-list-plc-hldr__details"></div>
       </div>
-    </>
+    </div>
   );
 };
 
