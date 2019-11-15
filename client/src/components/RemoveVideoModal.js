90173b5b259dcf57f001447a7921b3197661f019
John Rawlins <john.rawlins2@outlook.com>
2019-11-15 18:55:58 -0500

    Adjusted RemoveVideoModal styling and animation

--- client/src/components/RemoveVideoModal.js
+++ client/src/components/RemoveVideoModal.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Spring } from 'react-spring/renderprops';
 import '../css/remove-video-modal.scss';
 import MyVideoListContext from './context/my-video-list/myVideoListContext.js';
 
@@ -12,27 +13,36 @@ const RemoveVideoModal = () => {
   const handleSelection = event => {
     const btnSelection = event.target.textContent.toLowerCase();
     if (btnSelection === 'yes') {
-      removeVideoFromWatchList(removeVideoModal.videoID, removeVideoModal.videoImdbID);
+      removeVideoFromWatchList(
+        removeVideoModal.videoID,
+        removeVideoModal.videoImdbID
+      );
     } else {
       setRemoveVideoModal(false);
     }
   };
-  
 
   return (
     <div className="remove-video-modal">
-      <div className="modal-message">
-        <p className="modal-message__text">
-          Remove this item from your watch list?
-        </p>
-        <h1 className="modal-message__video-title">
-          {removeVideoModal.videoTitle}
-        </h1>
-        <div className="modal-btns" onClick={handleSelection}>
-          <button className="modal-btns__no">No</button>
-          <button className="modal-btns__yes">Yes</button>
-        </div>
-      </div>
+      <Spring
+        from={{ transform: 'scale(0)', opacity: 0 }}
+        to={{ transform: 'scale(1)', opacity: 1 }}
+      >
+        {animation => (
+          <div style={animation} className="modal-message">
+            <p className="modal-message__text">
+              Remove this item from your watch list?
+            </p>
+            <h1 className="modal-message__video-title">
+              {removeVideoModal.videoTitle}
+            </h1>
+            <div className="modal-btns" onClick={handleSelection}>
+              <button className="modal-btns__no">No</button>
+              <button className="modal-btns__yes">Yes</button>
+            </div>
+          </div>
+        )}
+      </Spring>
     </div>
   );
 };
