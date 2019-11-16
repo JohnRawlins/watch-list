0680900ea6708d6c2ac97457947163e329602c5f
John Rawlins <john.rawlins2@outlook.com>
2019-11-15 19:39:27 -0500

    Added animation to RemoveReviewModal

--- client/src/components/RemoveReviewModal.js
+++ client/src/components/RemoveReviewModal.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import ReviewContext from './context/review/reviewContext';
 import reviewStar from '../img/review-star.svg';
+import { Spring } from 'react-spring/renderprops';
 import '../css/remove-review-modal.scss';
 
 const RemoveReviewModal = () => {
@@ -33,37 +34,42 @@ const RemoveReviewModal = () => {
   const handleSelection = event => {
     if (event.target.value === 'no') {
       setDeleteReviewModal(false);
-    }
-
-    else{
+    } else {
       deleteVideoReview(review._id);
     }
   };
 
   return (
     <div className="remove-review-modal">
-      <div className="rm-review-modal-message">
-        <p className="rm-review-modal-message__text">
-          Are You Sure You Want To Remove This Review?
-        </p>
-        <div className="rm-review-media">
-          <span className="rm-review-media__date">
-            {formatReviewDate(review.date)}
-          </span>
-          <h1 className="rm-review-media__title">{review.videoTitle}</h1>
-          <div className="rm-review-score">
-            {createReviewStars(review.stars)}
+      <Spring
+        from={{ transform: 'scale(0)', opacity: 0 }}
+        to={{ transform: 'scale(1)', opacity: 1 }}
+      >
+        {animation => (
+          <div style={animation} className="rm-review-modal-message">
+            <p className="rm-review-modal-message__text">
+              Are You Sure You Want To Remove This Review?
+            </p>
+            <div className="rm-review-media">
+              <span className="rm-review-media__date">
+                {formatReviewDate(review.date)}
+              </span>
+              <h1 className="rm-review-media__title">{review.videoTitle}</h1>
+              <div className="rm-review-score">
+                {createReviewStars(review.stars)}
+              </div>
+            </div>
+            <div className="rm-review-modal-btns" onClick={handleSelection}>
+              <button className="rm-review-modal-btns__no" value="no">
+                No
+              </button>
+              <button className="rm-review-modal-btns__yes" value="yes">
+                Yes
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="rm-review-modal-btns" onClick={handleSelection}>
-          <button className="rm-review-modal-btns__no" value="no">
-            No
-          </button>
-          <button className="rm-review-modal-btns__yes" value="yes">
-            Yes
-          </button>
-        </div>
-      </div>
+        )}
+      </Spring>
     </div>
   );
 };
