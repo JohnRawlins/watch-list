831b02439bf82865a4b7b2a3945aeb6f24127ae1
John Rawlins <john.rawlins2@outlook.com>
2019-11-16 05:10:28 -0500

    Added FormError animations

--- client/src/components/FormError.js
+++ client/src/components/FormError.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Transition } from 'react-spring/renderprops';
 import '../css/form-error.scss';
 import formErrorIcon from '../img/form-error-icon.svg';
 
@@ -12,16 +13,26 @@ const FormError = ({ formErrors }) => {
   });
 
   return (
-    formErrors.length > 0 && (
-      <ul className="form-error">
-        <img
-          className="form-error__icon"
-          src={formErrorIcon}
-          alt="Form Error Icon"
-        />
-        <ul className="form-error-list">{formErrors}</ul>
-      </ul>
-    )
+    <Transition
+      items={formErrors}
+      keys={error => error.key}
+      from={{ opacity: 0, transform: 'scale(0)' }}
+      enter={{ opacity: 1, transform: 'scale(1)' }}
+      leave={{ opacity: 0, transform: 'scale(0)' }}
+    >
+      {error => props => (
+        <div className="form-error">
+          <img
+            className="form-error__icon"
+            src={formErrorIcon}
+            alt="Form Error Icon"
+          />
+          <ul style={props} className="form-error-list">
+            {error}
+          </ul>
+        </div>
+      )}
+    </Transition>
   );
 };
 
