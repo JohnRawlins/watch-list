ab925d83a18b5dffaebcf0c5eefa0906a5fad080
John Rawlins <john.rawlins2@outlook.com>
2020-07-17 14:24:50 -0400

    Added Cast to VideoProfile request

--- routes/video-profile.js
+++ routes/video-profile.js
@@ -1,13 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 const apiKey = process.env.OMDB_API_KEY;
+const tmdbApiKey = process.env.TMDB_API_KEY;
 
 // @route     GET api/video-profile
 // @desc      Retrieve profile for video
 // @access    Public
 
-router.get('/:videoTitle/:imdbID', async (req, res) => {
+const getMovieCast = async (videoID) => {
+  const movieCastLimit = 5;
+  try {
+    let response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${videoID}/credits?api_key=${tmdbApiKey}`
+    );
+
+    let movieCast = response.data.cast.filter((actor, index) => {
+      return index < movieCastLimit && actor.profile_path ? true : false;
+    });
+
+    return movieCast;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+router.get("/:videoTitle/:imdbID", async (req, res) => {
   try {
     const videoImdbID = req.params.imdbID;
 
@@ -16,15 +35,16 @@ router.get('/:videoTitle/:imdbID', async (req, res) => {
     );
 
     if (omdbResponse.data.Error) {
-      return res.status(400).json({ msg: 'Error loading video profile' });
+      return res.status(400).json({ msg: "Error loading video profile" });
+    } else {
+      omdbResponse.data.Cast = await getMovieCast(videoImdbID);
+      return res.status(200).json(omdbResponse.data);
     }
-
-    return res.status(200).json(omdbResponse.data);
   } catch (error) {
     console.error(error.message);
     return res.status(500).json({
       msg:
-        'The server was unable to process your request due to an internal error'
+        "The server was unable to process your request due to an internal error",
     });
   }
 });
