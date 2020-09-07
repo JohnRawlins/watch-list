bdc93eb6181d51c8cfb3f1ad98807884489abea0
John Rawlins <john.rawlins2@outlook.com>
2020-09-07 00:41:50 -0400

    Schedule Test

--- newsletter/test.js
+++ newsletter/test.js
@@ -2,11 +2,15 @@ const axios = require("axios");
 const PORT = process.env.PORT || 5000;
 
 const getNewReleases = async () => {
-  const websiteResponse = await axios.get(
-    `http://localhost:${PORT}/api/search/new-releases`
-  );
+  try {
+    const websiteResponse = await axios.get(
+      `http://localhost:${PORT}/api/search/new-releases`
+    );
 
-  console.log(websiteResponse.data);
+    console.log(websiteResponse.data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 getNewReleases();
