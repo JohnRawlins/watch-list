835390b625998f71ac553f238fc00bac57577e7a
John Rawlins <john.rawlins2@outlook.com>
2020-09-07 01:07:22 -0400

    Newsletter function

--- newsletter/test.js
+++ newsletter/test.js
@@ -1,15 +1,45 @@
 const axios = require("axios");
-const PORT = process.env.PORT || 5000;
+const FormData = require("form-data");
+const cheerio = require("cheerio");
+
+const createNewReleasesFormData = (formData) => {
+  formData.append("with_ott_providers", "384|43|37");
+  formData.append("certification", "PG-13|R");
+  formData.append("certification_country", "US");
+  formData.append("ott_region", "US");
+  formData.append("page", "1");
+  formData.append("sort_by", "primary_release_date.desc");
+  formData.append("vote_average.lte", "10");
+  formData.append("with_original_language", "en");
+  formData.append("with_runtime.gte", "0");
+  formData.append("with_runtime.lte", "400");
+  formData.append("language", "en-US");
+};
 
 const getNewReleases = async () => {
   try {
-    const websiteResponse = await axios.get(
-      `http://localhost:${PORT}/api/search/new-releases`
+    const newReleases = [];
+    const formData = new FormData();
+    createNewReleasesFormData(formData);
+    const websiteResponse = await axios.post(
+      "https://www.themoviedb.org/discover/movie",
+      formData,
+      { responseType: "text", headers: formData.getHeaders() }
     );
 
-    console.log(websiteResponse.data);
+    const $ = cheerio.load(websiteResponse.data);
+
+    $(".card").each(function (i, elem) {
+      const movieTitle = $(this).find("h2").text();
+      let moviePosterSrc = $(this).find(".poster").data("src");
+      if (movieTitle && moviePosterSrc) {
+        moviePosterSrc = "https:" + moviePosterSrc;
+        newReleases.push({ movieTitle, moviePosterSrc });
+      }
+    });
+    console.log(newReleases);
   } catch (error) {
-    console.log(error.message);
+    console.error(error);
   }
 };
 
